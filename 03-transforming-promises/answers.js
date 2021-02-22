dc63915b9eee3631dd4c7d4622a87afdea7a0da4
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-02-22 22:57:45 +0200

    finished

--- 03-transforming-promises/answers.js
+++ 03-transforming-promises/answers.js
@@ -1,48 +1,59 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {*} promise 
- * @param {*} transformer 
+ *
+ * @param {*} promise
+ * @param {*} transformer
  * @returns {Promise}
  */
-function mapPromise(promise, transformer){
+function mapPromise(promise, transformer) {
   return new Promise((resolve, reject) => {
     /* IMPLEMENT ME!! */
+    promise
+      .catch((err) => reject(err))
+      .then((value) => transformer(value))
+      .then((value) => resolve(value))
+      .catch((err) => reject(err));
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
-function squarePromise(numberPromise){
+function squarePromise(numberPromise) {
   return numberPromise
-    .then(/* IMPLEMENT ME! */);
+    .catch((err) => {
+      return Promise.reject(err);
+    })
+    .then((val) => {
+      temp = +val;
+      if (!isNaN(temp)) return temp * temp;
+      return Promise.reject("Cannot convert '" + val + "' to a number!");
+    });
 }
 
 /**
  * EXERCISE 3
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
-function squarePromiseOrZero(promise){
-  return squarePromise(promise)
-    .catch(/* IMPLEMENT ME! */);
+function squarePromiseOrZero(promise) {
+  return squarePromise(promise).catch(()=>0);
 }
 
 /**
  * EXERCISE 4
- * 
- * @param {Promise} promise 
+ *
+ * @param {Promise} promise
  * @returns {Promise}
  */
-function switcheroo(promise){
-  return promise.then(/* IMPLEMENT ME */);
+function switcheroo(promise) {
+  return promise.then((res)=>Promise.reject(res), rej=>rej );
 }
 
 /**
